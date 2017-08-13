f868c26caf0ec8db7cae7e730fda322f36290ae0
Алексей Павленко <sk.alexey.pavlenko@gmail.com>
2017-08-13 20:54:59 +0300

    put together the global ControllerAdvice

--- src/test/java/guru/springframework/controllers/ImageControllerTest.java
+++ src/test/java/guru/springframework/controllers/ImageControllerTest.java
@@ -22,9 +22,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class ImageControllerTest {
 
@@ -42,7 +40,7 @@ public class ImageControllerTest {
 	public void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
 		imageController = new ImageController(imageService, recipeService);
-		mockMvc = MockMvcBuilders.standaloneSetup(imageController).build();
+		mockMvc = MockMvcBuilders.standaloneSetup(imageController).setControllerAdvice(new ControllerExceptionHandler()).build();
 
 	}
 
@@ -101,6 +99,13 @@ public class ImageControllerTest {
 		byte[] contentAsByteArray = mockHttpServletResponse.getContentAsByteArray();
 
 		assertEquals(s.getBytes().length, contentAsByteArray.length);
+	}
+
+	@Test
+	public void testImageNumberFormatException() throws Exception {
+		mockMvc.perform(get("/recipe/asdasd/recipeimage"))
+				.andExpect(status().isBadRequest())
+				.andExpect(view().name("400error"));
 	}
 
 }
