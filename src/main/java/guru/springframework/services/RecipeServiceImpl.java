9ad0de514482fe3701285cc1eec8ff0f35ae715b
Алексей Павленко <sk.alexey.pavlenko@gmail.com>
2017-08-13 20:39:56 +0300

    join a new class for handle a number format exception

--- src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -41,12 +41,14 @@ public class RecipeServiceImpl implements RecipeService {
     }
 
     @Override
-    public Recipe findById(Long l) {
+    public Recipe findById(Long id) {
 
-        Optional<Recipe> recipeOptional = recipeRepository.findById(l);
+
+
+        Optional<Recipe> recipeOptional = recipeRepository.findById(id);
 
         if (!recipeOptional.isPresent()) {
-            throw new NotFoundException("Recipe Not Found!");
+            throw new NotFoundException("Recipe Not Found. For ID value: " + id.toString());
         }
 
         return recipeOptional.get();
